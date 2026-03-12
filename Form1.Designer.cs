8d880c9fbd61d42748f732c16ba34e6603a81177
kim dg <dgkim729@gmail.com>
2026-03-12 13:01:06 +0900

    폼에 '나를 잡아봐' 버튼 및 이동 기능 추가
    폼에 btnTarget 버튼을 추가하고, 마우스 오버 시 폼 내에서 랜덤 위치로 이동하는 기능을 구현했습니다. 버튼의 위치는 폼 제목 표시줄에 좌표로 표시됩니다. Form1.resx 파일은 주석 포맷만 일부 변경되었습니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnTarget = new Button();
+            SuspendLayout();
+            // 
+            // btnTarget
+            // 
+            btnTarget.BackColor = Color.FromArgb(255, 255, 192);
+            btnTarget.Font = new Font("한컴 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnTarget.ForeColor = Color.Blue;
+            btnTarget.Location = new Point(244, 150);
+            btnTarget.Name = "btnTarget";
+            btnTarget.Size = new Size(300, 120);
+            btnTarget.TabIndex = 0;
+            btnTarget.Text = "나를 잡아봐";
+            btnTarget.UseVisualStyleBackColor = false;
+            btnTarget.MouseEnter += btnTarget_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnTarget);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnTarget;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,27 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void btnTarget_MouseEnter(object sender, EventArgs e)
+        {
+            // 1. 난수 생성기 준비
+            Random rd = new Random();
+
+            // 2. 가용 영역 계산 (버튼이 폼 테두리에 걸리지 않게 보호)
+            // ClientSize는 타이틀 바와 테두리를 제외한 실제 흰 도화지 영역임
+            int maxX = this.ClientSize.Width;
+            int maxY = this.ClientSize.Height;
+
+            // 3. 랜덤 좌표 추출 (0 ~ 최대 가용치-버튼 크기 사이)
+            int nextX = rd.Next(0, maxX-300);
+            int nextY = rd.Next(0, maxY-120);
+
+            // 4. 위치 할당 (새로운 Point 객체 생성)
+            btnTarget.Location = new Point(nextX, nextY);
+
+            // 5. 시각적 피드백 (폼 제목 표시줄에 좌표 출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+        }
+
     }
 }
